3cedfdd6f486de894eb09b451b829924315c8798
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-16 21:21:44 +0530

    k

---
+++ q_8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class q_8 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        String c=(n>0)?"positive":"negative";
+        System.out.println(c);
+
+    }
+    
+}
